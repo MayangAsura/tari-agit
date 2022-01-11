e1285634ea2e8a54f4965fcb4bb33d75a2474b57
MayangAsura <mayang.asura123@gmail.com>
2022-01-11 10:39:42 +0700

    bismillah added first commit

---
+++ application/views/app.php
@@ -0,0 +1,285 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Undangan Pernikahan Tari &amp; Agit</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Undangan Pernikahan Tari & Agit" />
+	<meta name="keywords" content="wedding invitation, mayang, asura, mayangasura, developer, wedding, responsive" />
+	<meta name="author" content="Mayang_A" />
+
+  <!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FREEHTML5.CO
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+	
+	<link href="https://fonts.googleapis.com/css?family=Montez" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="<?= base_url() ?>assets/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="<?= base_url() ?>assets/css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="<?= base_url() ?>assets/css/bootstrap.css">
+	<!-- Superfish -->
+	<link rel="stylesheet" href="<?= base_url() ?>assets/css/superfish.css">
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="<?= base_url() ?>assets/css/magnific-popup.css">
+	
+	<link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+
+
+	<!-- Modernizr JS -->
+	<script src="<?= base_url() ?>assets/js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		<div id="fh5co-wrapper">
+		<div id="fh5co-page">
+	
+		<div class="fh5co-hero" data-section="home">
+			<div class="fh5co-overlay"></div>
+			<div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url(assets/images/bg1.jpg);">
+				<div class="display-t">
+					<div class="display-tc">
+						<div class="container">
+							<div class="col-md-10 col-md-offset-1">
+								<div class="animate-box">
+									<h1>Pernikahan</h1>
+									<h2>Tari &amp; Agit</h2>
+									<p><span>01.12.2021</span></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<header id="fh5co-header-section" class="sticky-banner">
+			<div class="container">
+				<div class="nav-header">
+					<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle dark"><i></i></a>
+					<h1 id="fh5co-logo"><a href="index.html">Undangan</a></h1>
+					<!-- START #fh5co-menu-wrap -->
+					<nav id="fh5co-menu-wrap" role="navigation">
+						<ul class="sf-menu" id="fh5co-primary-menu">
+							<li class="active"><a href="index.html" >Home</a></li>
+							<li>
+								<a href="#fh5co-couple" class="fh5co-sub-ddown">Kedua Mempelai</a>
+								<!-- <ul class="fh5co-sub-menu">
+									<li><a href="#">Dropdown Menu 1</a></li>
+									<li><a href="#">Dropdown Menu 2</a></li>
+									<li><a href="#">Dropdown Menu 3</a></li>
+									<li><a href="#">Dropdown Menu 4</a></li>
+								</ul> -->
+							</li>
+							<li><a href="#fh5co-when-where">Waktu &amp; Lokasi</a></li>
+							<li><a href="guest.html">Guest</a></li>
+							<li><a href="gallery.html">Galeri</a></li>
+							<li><a href="blog.html">Cerita Kita</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</header>
+
+		<!-- end:header-top -->
+		
+		<div id="fh5co-couple" class="fh5co-section-gray">
+			<div class="container">
+				<div class="row row-bottom-padded-md animate-box">
+					<div class="col-md-6 col-md-offset-3 text-center">
+						<div class="col-md-5 col-sm-5 col-xs-5 nopadding">
+							<img src="images/groom.jpg" class="img-responsive" alt="Fitra Guntari Profile">
+							<h3>Fitra Guntari</h3>
+						</div>
+						<div class="col-md-2 col-sm-2 col-xs-2 nopadding"><h2 class="amp-center"><i class="icon-heart"></i></h2></div>
+						<div class="col-md-5 col-sm-5 col-xs-5 nopadding">
+							<img src="images/bride.jpg" class="img-responsive" alt="Agit Profile">
+							<h3>Agit</h3>
+						</div>
+					</div>
+				</div>
+				<div class="row animate-box">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="col-md-12 text-center heading-section">
+							<h2>Tanggal Resepsi</h2>
+							<p><strong>28 November 2021 &mdash; Padang Marapalam, Lengayang, Pesisir Selatan</strong></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="fh5co-countdown">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center animate-box">
+					<p class="countdown">
+						<span id="days"></span>
+						<span id="hours"></span>
+						<span id="minutes"></span>
+						<span id="seconds"></span>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div id="fh5co-when-where" class="fh5co-section-gray">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+						<h2>Waktu &amp; Lokasi</h2>
+					</div>
+				</div>
+				<div class="row row-bottom-padded-md">
+					<div class="col-md-6 text-center animate-box">
+						<div class="wedding-events">
+							<div class="ceremony-bg" style="background-image: url(<?= base_url('assets/images/akad.jpg')?>);"></div>
+							<div class="desc">
+								<h3>Akad Nikah</h3>
+								<p><strong>Sabtu, 28 November 2021, 08:88 - 11:00 WIB </strong></p>
+								<p>Lokasi : Sebelah Panti Asuhan, Kecamatan Lengayang, Kabupaten Pesisir Selatan, Sumatera Barat</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 text-center animate-box">
+						<div class="wedding-events">
+							<div class="ceremony-bg" style="background-image: url(<?= base_url('assets/images/baralek.jpg')?>);"></div>
+							<div class="desc">
+								<h3>Resepsi Pernikahan</h3>
+								<p><strong>Ahad, 29 November 2021, 08:88 - 11:00 WIB</strong></p>
+								<p>Lokasi : Sebelah Panti Asuhan, Kecamatan Lengayang, Kabupaten Pesisir Selatan, Sumatera Barat</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+					<!-- width="800" height="600" -->
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1994.0197947681645!2d100.71865979479085!3d-1.710930307952025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e2cd15b196fcc61%3A0x696c655020a1fb43!2sPANTI%20ASUHAN%20ANAK%20YATIM!5e0!3m2!1sen!2sid!4v1630136607856!5m2!1sen!2sid" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+						<!-- <div id="map" class="fh5co-map"></div> -->
+						<!-- <iframe src="https://www.google.com/maps/place/Lengayang,+South+Pesisir+Regency,+West+Sumatra/@-1.7106386,100.7193621,49m/data=!3m1!1e3!4m5!3m4!1s0x2e2cc5dd3e34a7bb:0x4039d80b22106b0!8m2!3d-1.5317693!4d100.8451467" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe> -->
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div id="fh5co-started" style="background-image:url(<?= base_url('assets/images/bg3.jpg')?>);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+			<div class="container">
+				<div class="row animate-box">
+					<div class="col-md-8 col-md-offset-2 text-center heading-section">
+						<h2>Konfirmasi Kehadiran</h2>
+						<p>Mohon Isi Data Berikut dan Tulis Ucapan untuk Kedua Mempelai  </p>
+						<!-- <p>Please Fill-up the form to notify you that you're attending. Thanks. -->
+					</div>
+				</div>
+				<div class="row animate-box">
+					<div class="col-md-10 col-md-offset-1">
+						<form class="form-inline">
+							<div class="col-md-6 col-sm-4">
+								<div class="form-group">
+									<label for="name" class="sr-only">Nama / Instansi</label>
+									<input type="name" class="form-control" id="name" placeholder="Nama / Instansi" required>
+								</div>
+							</div>
+							<div class="col-md-6 col-sm-4">
+								<div class="form-group">
+									<label for="email" class="sr-only">No. Hp</label>
+									<input type="email" class="form-control" id="email" placeholder="No. Hp">
+								</div>
+							</div>
+							<!-- <div class="col-md-4 col-sm-4">
+								<button type="submit" class="btn btn-primary btn-block">I am Attending</button>
+							</div> -->
+						</form>
+					</div>
+				</div>
+				<div class="row animate-box">
+					<div class="col-md-10 col-md-offset-1">
+						<form class="form-inline">
+							<div class="col-md-12 col-sm-4">
+								<div class="form-group">
+									<label for="name" class="sr-only">Ucapan</label>
+									<textarea type="name" class="form-control" id="name" placeholder="Ucapan"></textarea>
+								</div>
+							</div>
+							<div class="col-md-4 col-sm-4">
+								<button type="submit" class="btn btn-primary btn-block">Saya Hadir</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<footer>
+			<div id="footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 text-center">
+							<h2>Tari &amp; Agit</h2>
+						</div>
+						<div class="col-md-6 col-md-offset-3 text-center">
+							<p class="fh5co-social-icons">
+								<a href="#"><i class="icon-twitter2"></i></a>
+								<a href="#"><i class="icon-facebook2"></i></a>
+								<a href="#"><i class="icon-instagram"></i></a>
+								<a href="#"><i class="icon-dribbble2"></i></a>
+								<a href="#"><i class="icon-youtube"></i></a>
+							</p>
+							<p>Copyright 2016 Free Html5 <a href="#">Module</a>. All Rights Reserved. <br>Made with <i class="icon-heart3"></i> by <a href="http://freehtml5.co/" target="_blank">Freehtml5.co</a> / Demo Images: <a href="https://unsplash.com/" target="_blank">Unsplash</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+	
+
+	</div>
+	<!-- END fh5co-page -->
+
+	</div>
+	<!-- END fh5co-wrapper -->
+
+	<!-- Google Map -->
+	<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script> -->
+	<!-- jQuery -->
+	<script src="<?= base_url() ?>assets/dist/scripts.min.js"></script>
+	</body>
+</html>
+
